81b8f006087fae8c1c09e22ccb25866984fdaf0a
Kot <kotvladimir89@mail.ru>
2022-11-07 20:33:38 +0500

    add Examples rekyrsia

---
+++ Seminar005_/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i < 4; i++) // index = 0; длинна линии 4; индекс + 1
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write(arr[index] + " ");
+        index++;
+    }
+    Console.WriteLine();
+
+
+
+
